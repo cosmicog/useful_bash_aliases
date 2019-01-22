1e58238cf51b14abdf404cb6141db419a291bddd
Orhan G. Hafif <orhangazi4401@gmail.com>
2019-01-22 10:13:42 +0300

    Create aliases.sh

---
+++ aliases.sh
@@ -0,0 +1,15 @@
+# some more ls aliases
+alias ll='ls -alF'
+alias la='ls -la'
+alias l='ls -CF'
+
+# custom aliases
+alias die_mothafucka='sudo shutdown now'
+alias gopy_output='| xclip -selection clipboard'
+alias asd='source ~/.bashrc && source ~/px4_firm_source.sh && roslaunch drone_gazebo april_tags_mania.launch'
+alias dfg='roslaunch drone_bringup gazebo_bringup.launch'
+alias qtcc='qtcreator-ros -client'
+alias sob='source ~/.bashrc'
+alias nabrc_n_m='nano ~/.bashrc'
+alias catbashrc='colorcat ~/.bashrc'
+alias adamim_ardu_tasakli='sim_vehicle.py -N -v ArduCopter -f gazebo-iris -m --mav10 -I0'
