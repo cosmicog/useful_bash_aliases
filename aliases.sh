412b32087aab2ace0300e89952f552088b690157
cosmicog <orhangazi4401@gmail.com>
2019-01-24 10:00:07 +0300

    Fix mistake

--- aliases.sh
+++ aliases.sh
@@ -5,7 +5,7 @@ alias l='ls -CF'
 
 # custom aliases
 alias die_mothafucka='sudo shutdown now'
-alias gopy_output='| xclip -selection clipboard'
+alias gopy_output='xclip -selection clipboard'
 alias asd='source ~/.bashrc && source ~/px4_firm_source.sh && roslaunch drone_gazebo april_tags_mania.launch'
 alias dfg='roslaunch drone_bringup gazebo_bringup.launch'
 alias qtcc='qtcreator-ros -client'
